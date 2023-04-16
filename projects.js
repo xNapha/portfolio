77f51ece88faf7212b349d71c595167f6a9ac9b3
xNapha <nathancai1997@gmail.com>
2023-04-16 15:09:50 +1000

    updating projects object

--- projects.js
+++ projects.js
@@ -1,25 +1,25 @@
 //importing projects from data file
 const projects = [
     {
-        title: "Project 1",
-        description: "Some Description",
-        gitHubLink: "",
-        websiteLink: "",
-        image: "./../assets/project-images/",
+        title: "App-Who-le OS",
+        description: "Recreating Mac OS on a browser",
+        gitHubLink: "https://github.com/xNapha/fake-os",
+        websiteLink: "https://xnapha.github.io/fake-os/",
+        image: "./../assets/project-images/fake-os.png",
     },
     {
-        title: "Project 2",
-        description: "Some Description",
-        gitHubLink: "",
-        websiteLink: "",
-        image: "./../assets/project-images/",
+        title: "Morse Code Translator",
+        description: "Translate the popular encrypted alphabet, morse code.",
+        gitHubLink: "https://github.com/xNapha/morse-code-translator",
+        websiteLink: "https://xnapha.github.io/morse-code-translator/",
+        image: "./../assets/project-images/morse-code-translator.png",
     },
     {
-        title: "Project 3",
-        description: "Some Description",
-        gitHubLink: "",
-        websiteLink: "",
-        image: "./../assets/project-images/",
+        title: "Google Books",
+        description: "Search engine only for books",
+        gitHubLink: "https://github.com/xNapha/google-books",
+        websiteLink: "https://xnapha.github.io/google-books/",
+        image: "./../assets/project-images/google-books.png",
     },
     {
         title: "Project 4",
@@ -30,6 +30,13 @@ const projects = [
     },
     {
         title: "Project 5",
+        description: "Some Description",
+        gitHubLink: "",
+        websiteLink: "",
+        image: "./../assets/project-images/",
+    },
+    {
+        title: "Project 6",
         description: "Some Description",
         gitHubLink: "",
         websiteLink: "",
@@ -48,10 +55,10 @@ const renderProjects = (section, projects) => {
             <h4>${project.title}</h4>
             <p>${project.description}</p>
             <div class="icon-container">
-                <a href="${project.gitHubLink}">
+                <a href="${project.gitHubLink}" target="_blank">
                     <i class="fa-brands fa-github"></i>
                 </a>
-                <a href="${project.websiteLink}">
+                <a href="${project.websiteLink}" target="_blank">
                     <i class="fa-solid fa-paper-plane"></i>
                 </a>
             </div>
